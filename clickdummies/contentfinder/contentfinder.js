31eac98bb65e4fdb714f2e9b4e5b99a7377b83c8
Roché Compaan <roche@upfrontsystems.co.za>
2012-10-21 07:38:07 +0200

    add contentfinder (aka referencebrowswer) click dummy

---
+++ clickdummies/contentfinder/contentfinder.js
@@ -0,0 +1,39 @@
+var ContentFinder = function(id, path) {
+    var self = this;
+    this.id = id;
+    this.container = $(id);
+    this.dropdown = $('.chzn-drop', this.container);
+    this.results = $('.chzn-results', this.container);
+    this.input = $('.search-field input', this.container);
+    $(this.input).toggle(function() {
+        self.dropdown.css({'left': 0});
+    }, function() {
+        self.dropdown.css({'left': -9000});
+    });
+    this.current_path = path;
+};
+
+ContentFinder.prototype.listdir = function(path) {
+    var html = [];
+    if (path == '/Plone') {
+        html.push(
+            '<li data-path="/Plone/front-page" class="active-result"><span data-uid="811793d984af4c2dbdf4db2080df1e27" class="contenttype-document">Welcome to Plone</span></li>'
+        )
+        html.push(
+            '<li data-path="/Plone/news" class="active-result"><span data-uid="811793d984af4c2dbdf4db2080df1e27" class="contenttype-folder">News</span></li>'
+        )
+        html.push(
+            '<li data-path="/Plone/events" class="active-result"><span data-uid="811793d984af4c2dbdf4db2080df1e27" class="contenttype-folder">Events</span></li>'
+        )
+        html.push(
+            '<li data-path="/Plone/Members" class="active-result"><span data-uid="811793d984af4c2dbdf4db2080df1e27" class="contenttype-folder">Users</span></li>'
+        )
+    }
+    this.results.html(html.join(''));
+}
+
+
+$(document).ready(function () {
+    var finder = new ContentFinder('#related_items_finder', '/Plone');
+    finder.listdir('/Plone');
+});
