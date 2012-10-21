35ef55f6edd0f6d27e6a080528a0d298086262ad
Roché Compaan <roche@upfrontsystems.co.za>
2012-10-21 16:01:03 +0200

    add data for news and events folders

--- clickdummies/contentfinder/finderdata.js
+++ clickdummies/contentfinder/finderdata.js
@@ -52,55 +52,252 @@ var finderdata = {
         ]
     },
     "http://localhost:8080/Plone/news": {
-        "parent_url": "",
+        "parent_url": "http://plone.org",
         "path": [
-            {"url": "http://localhost:8080/Plone",
-            "icon": null,
-            "title": "Home"}
+            {"url": "/plone.org", "title": "Plone CMS: Open Source Content Management"},
+            {"url": "http://plone.org/news",
+"title": "News"}
+        ],
+        "upload_allowed": false,
+        "items": [
+            {
+            "uid": "0306b0bd09895492a7b05ca0998959d2",
+            "title": "Plone passes the 100.000 download mark",
+            "url": "http://plone.org/news/100k-downloads",
+            "is_folderish": false, "portal_type": "News Item",
+            "icon": "",
+            "id": "100k-downloads",
+            "normalized_type": "news-item"
+            },
+            {
+            "uid": "87d6ab60c67c02336fc26a5cabe9af3d",
+            "title": "Plone is 1 year old",
+            "url": "http://plone.org/news/1yearold",
+            "is_folderish": false, "portal_type": "News Item",
+            "icon": "",
+            "id": "1yearold",
+            "normalized_type": "news-item"
+            },
+            {
+            "uid": "52c5b15d54a943e53280c039022a193f",
+            "title": "Plone 2.0.4 Released",
+            "url": "http://plone.org/news/2.0.4",
+            "is_folderish": false, "portal_type": "News Item",
+            "icon": "",
+            "id": "2.0.4",
+            "normalized_type": "news-item"
+            },
+            {
+            "uid": "db29c7a8e22fe0799b8517be7a2cc044",
+            "title": "Planned plone.org outage",
+            "url": "http://plone.org/news/20050219-outage",
+            "is_folderish": false, "portal_type": "News Item",
+            "icon": "",
+            "id": "20050219-outage",
+            "normalized_type": "news-item"
+            },
+            {
+            "uid": "cddef9bdb91c9e25d4ca4e8b1da51112",
+            "title": "2010 Plone Help Channel Superstar Awards",
+            "url": "http://plone.org/news/2010-plone-help-channel-superstar-awards",
+            "is_folderish": false, "portal_type": "News Item",
+            "icon": "",
+            "id": "2010-plone-help-channel-superstar-awards",
+            "normalized_type": "news-item"
+            },
+            {
+            "uid": "f657ab65fed073c94b29cdd07208c71c",
+            "title": "2010 Plone Help Channel Superstars Results",
+            "url": "http://plone.org/news/2010-plone-irc-superstars-results",
+            "is_folderish": false, "portal_type": "News Item",
+            "icon": "",
+            "id": "2010-plone-irc-superstars-results",
+            "normalized_type": "news-item"
+            },
+            {
+            "uid": "bed770fcfab569ccc7f342562f14bf11",
+            "title": "Plone Foundation Board for 2011-2012 Elected",
+            "url": "http://plone.org/news/2011-2012-foundation-board",
+            "is_folderish": false, "portal_type": "News Item",
+            "icon": "",
+            "id": "2011-2012-foundation-board",
+            "normalized_type": "news-item"},
+            {
+            "uid": "0ab1a93517d3ea8238c5e2aaa9664d83",
+            "title": "2011 Plone Conference Adopts Anti-Harassment Policy",
+            "url": "http://plone.org/news/2011PloneConferenceAnti-HarassmentPolicy",
+            "is_folderish": false, "portal_type": "News Item",
+            "icon": "",
+            "id": "2011PloneConferenceAnti-HarassmentPolicy",
+            "normalized_type": "news-item"
+            },
+            {
+            "uid": "f0228e9e2f50ecf933440f1756c3180a",
+            "title": "San Francisco to Host 2011 Plone Conference",
+            "url": "http://plone.org/news/2011PloneConferenceResults",
+            "is_folderish": false, "portal_type": "News Item",
+            "icon": "",
+            "id": "2011PloneConferenceResults",
+            "normalized_type": "news-item"
+            },
+            {
+            "uid": "4c1b79524125564e400c87031b157b6e",
+            "title": "2011 Plone Conference Has Sold 100 Tickets",
+            "url": "http://plone.org/news/2011ploneconf100tickets",
+            "is_folderish": false, "portal_type": "News Item",
+            "icon": "",
+            "id": "2011ploneconf100tickets",
+            "normalized_type": "news-item"
+            },
+            {
+            "uid": "42bf0c15cf5bfa29aec59938b13816d5",
+            "title": "Plone Foundation Begins Search for Plone Conference 2011 Venue",
+            "url": "http://plone.org/news/2011ploneconfcallforproposals",
+            "is_folderish": false, "portal_type": "News Item",
+            "icon": "",
+            "id": "2011ploneconfcallforproposals",
+            "normalized_type": "news-item"
+            },
+            {
+            "uid": "8441a630f46fb0a2b714ac76a58a4f60",
+            "title": "2011 Plone Conference Now Accepting Talk Submissions",
+            "url": "http://plone.org/news/2011ploneconfcallfortalks",
+            "is_folderish": false, "portal_type": "News Item",
+            "icon": "",
+            "id": "2011ploneconfcallfortalks",
+            "normalized_type": "news-item"
+            },
+            {
+            "uid": "34b5b7cf0fda4f6a9ae18da5d348771a",
+            "title": "Plone Foundation announces Plone Conference 2013 selection process",
+            "url": "http://plone.org/news/2013ploneconf",
+            "is_folderish": false, "portal_type": "News Item",
+            "icon": "",
+            "id": "2013ploneconf",
+            "normalized_type": "news-item"
+            },
+            {
+            "uid": "878aa250543011d123775e6705347213",
+            "title": "29 contributions sponsored by Google",
+            "url": "http://plone.org/news/29-contributions-sponsored-by-google",
+            "is_folderish": false, "portal_type": "News Item",
+            "icon": "",
+            "id": "29-contributions-sponsored-by-google",
+            "normalized_type": "news-item"
+            },
+        ]
+    },
+    "http://localhost:8080/Plone/events": {
+        "parent_url": "http://plone.org",
+        "path": [
+            {
+            "url": "/plone.org",
+            "title": "Plone CMS: Open Source Content Management"
+            },
+            {
+            "url": "http://plone.org/events",
+            "title": "Events"}
             ],
         "upload_allowed": false,
         "items": [
             {
-                "description": "Congratulations! You have successfully installed Plone.",
-                "uid": "811793d984af4c2dbdf4db2080df1e27",
-                "title": "Welcome to Plone",
-                "url": "http://localhost:8080/Plone/front-page",
-                "is_folderish": false,
-                "portal_type": "Document",
-                "icon": null,
-                "id": "front-page",
-                "normalized_type": "document"
+            "uid": "eb9480a74c375da85abada276b4dcf0c",
+            "title": "Community Events",
+            "url": "http://plone.org/events/community",
+            "is_folderish": true, "portal_type": "Folder",
+            "icon": "",
+            "id": "community",
+            "normalized_type": "folder"
             },
             {
-                "description": "Site News",
-                "uid": "ad59bb63e1d648fea9ec04818289a271",
-                "title": "News",
-                "url": "http://localhost:8080/Plone/news",
-                "is_folderish": true, "portal_type": "Folder",
-                "icon": null,
-                "id": "news",
-                "normalized_type": "folder"
+            "uid": "d7695d3bc0197473922c459ce817ed46",
+            "title": "Training Announcements",
+            "url": "http://plone.org/events/training",
+            "is_folderish": true, "portal_type": "Topic",
+            "icon": "",
+            "id": "training",
+            "normalized_type": "topic"
             },
             {
-                "description": "Site Events",
-                "uid": "5e745db171614119af59be55ab12c425",
-                "title": "Events",
-                "url": "http://localhost:8080/Plone/events",
-                "is_folderish": true, "portal_type": "Folder",
-                "icon": null,
-                "id": "events",
-                "normalized_type": "folder"
+            "uid": "5c7dd4fc577caedda65aaaca87ff139a",
+            "title": "Conferences",
+            "url": "http://plone.org/events/conferences",
+            "is_folderish": true, "portal_type": "Folder",
+            "icon": "",
+            "id": "conferences",
+            "normalized_type": "folder"
             },
             {
-                "description": "Site Users",
-                "uid": "47cb04f38c9345f9a634c6c529382688",
-                "title": "Users",
-                "url": "http://localhost:8080/Plone/Members",
-                "is_folderish": true, "portal_type": "Folder",
-                "icon": null,
-                "id": "Members",
-                "normalized_type": "folder"
+            "uid": "63b2efc00770f6de73848416ae6ffba2",
+            "title": "Regional Symposia",
+            "url": "http://plone.org/events/regional",
+            "is_folderish": true, "portal_type": "Folder",
+            "icon": "",
+            "id": "regional",
+            "normalized_type": "folder"
             },
-        ]
-    },
+            {
+            "uid": "12810441087e4bece063db9a1f1c0b5a",
+            "title": "Sprints",
+            "url": "http://plone.org/events/sprints",
+            "is_folderish": true, "portal_type": "Folder",
+            "icon": "",
+            "id": "sprints",
+            "normalized_type": "folder"
+            },
+            {
+            "uid": "5551a55592d3ca3124582da34331edcc",
+            "title": "Summer of Code",
+            "url": "http://plone.org/events/summer-of-code",
+            "is_folderish": true, "portal_type": "Folder",
+            "icon": "",
+            "id": "summer-of-code",
+            "normalized_type": "folder"
+            },
+            {
+            "uid": "a9207ea9e517be3e92482a86d483e121",
+            "title": "World Plone Day",
+            "url": "http://plone.org/events/wpd",
+            "is_folderish": true, "portal_type": "Folder",
+            "icon": "",
+            "id": "wpd",
+            "normalized_type": "folder"
+            },
+            {
+            "uid": "91b9c13e4ed305a08470db6ea31fad2d",
+            "title": "Plone Tune-Up",
+            "url": "http://plone.org/events/tuneup.jpg",
+            "is_folderish": false, "portal_type": "Image",
+            "icon": "image.png",
+            "id": "tuneup.jpg",
+            "normalized_type": "image"
+            },
+            {
+            "uid": "82455803dae603f842a953c72da614c1",
+            "title": "Plone Tune-Ups",
+            "url": "http://plone.org/events/plone-tuneups",
+            "is_folderish": true, "portal_type": "Folder",
+            "icon": "",
+            "id": "plone-tuneups",
+            "normalized_type": "folder"
+            },
+            {
+            "uid": "8d597a41b46c36dfa63c28010fb632d8",
+            "title": "Plone Strategic Planning Summit 2008",
+            "url": "http://plone.org/events/2008-summit",
+            "is_folderish": true, "portal_type": "Folder",
+            "icon": "",
+            "id": "2008-summit",
+            "normalized_type": "folder"
+            },
+            {
+            "uid": "7d09a2c25a840b0b9798383947611466",
+            "title": "50th Plone Tune-Up Tshirt",
+            "url": "http://plone.org/events/50th-plone-tuneup-tshirt",
+            "is_folderish": false, "portal_type": "Image",
+            "icon": "png.png",
+            "id": "50th-plone-tuneup-tshirt",
+            "normalized_type": "image"}
+            ]
+    }
 }
