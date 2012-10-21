23f35e4c5f73bc2efb72c15b5fcc7be88ed4f232
Roché Compaan <roche@upfrontsystems.co.za>
2012-10-21 12:57:25 +0200

    implement select_item

--- clickdummies/contentfinder/contentfinder.js
+++ clickdummies/contentfinder/contentfinder.js
@@ -1,13 +1,14 @@
-var ContentFinder = function(id, path) {
+var ContentFinder = function(id, path, multiselect) {
     var self = this;
     this.id = id;
     this.container = $(id);
+    this.multiselect = multiselect;
+    this.selecteditems = [];
     this.dropdown = $('.chzn-drop', this.container);
     this.results = $('.chzn-results', this.container);
     this.input = $('.search-field input', this.container);
     this.input.attr('value', this.input.attr('data-placeholder'));
     $(this.input).toggle(function() {
-        console.log('toggle');
         if (self.input.attr('value') == self.input.attr('data-placeholder')) {
             self.input.attr('value', '');
         };
@@ -22,6 +23,7 @@ var ContentFinder = function(id, path) {
 };
 
 ContentFinder.prototype.listdir = function(path) {
+    var self = this;
     var html = [];
     var items = finderdata[path].items;
     for (var i=0; i<items.length; i++) {
@@ -34,7 +36,30 @@ ContentFinder.prototype.listdir = function(path) {
         )
     }
     this.results.html(html.join(''));
+    $('li', this.results)
+        .unbind('.finderresult')
+        .bind('click.finderresult', function() {
+            self.select_item($(this))
+            });
 }
+
+ContentFinder.prototype.select_item = function(item) {
+    var self = this;
+    // remove previously selected items from the list
+    var new_selecteditems = [];
+    for (var i=0; i<self.selecteditems.length; i++) {
+        var selected = self.selecteditems[i]; 
+        if (selected.attr('data-uid') != item.attr('data-uid')) {
+            new_selecteditems.push(selected);
+            if (selected.hasClass('selected')) {
+                selected.toggleClass('selected');
+            }
+        }
+    }
+    new_selecteditems.push(item);
+    item.toggleClass('selected');
+    self.selecteditems = new_selecteditems;
+};
 
 
 $(document).ready(function () {
