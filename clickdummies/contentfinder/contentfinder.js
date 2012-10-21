e57162f5277eed4e9da923707ec6142359104f17
Roché Compaan <roche@upfrontsystems.co.za>
2012-10-21 12:06:09 +0200

    move hardcoded paths into finderdata.js and root url to finder div in html
    refactor contentfinder.js to use finderdata.js. in practice,
    finderdata.js will be replaced with AJAX calls to the server

--- clickdummies/contentfinder/contentfinder.js
+++ clickdummies/contentfinder/contentfinder.js
@@ -7,6 +7,7 @@ var ContentFinder = function(id, path) {
     this.input = $('.search-field input', this.container);
     this.input.attr('value', this.input.attr('data-placeholder'));
     $(this.input).toggle(function() {
+        console.log('toggle');
         if (self.input.attr('value') == self.input.attr('data-placeholder')) {
             self.input.attr('value', '');
         };
@@ -22,18 +23,14 @@ var ContentFinder = function(id, path) {
 
 ContentFinder.prototype.listdir = function(path) {
     var html = [];
-    if (path == '/Plone') {
-        html.push(
-            '<li data-path="/Plone/front-page" class="active-result"><span data-uid="811793d984af4c2dbdf4db2080df1e27" class="contenttype-document">Welcome to Plone</span></li>'
-        )
-        html.push(
-            '<li data-path="/Plone/news" class="active-result"><span data-uid="811793d984af4c2dbdf4db2080df1e27" class="contenttype-folder">News</span></li>'
-        )
-        html.push(
-            '<li data-path="/Plone/events" class="active-result"><span data-uid="811793d984af4c2dbdf4db2080df1e27" class="contenttype-folder">Events</span></li>'
-        )
-        html.push(
-            '<li data-path="/Plone/Members" class="active-result"><span data-uid="811793d984af4c2dbdf4db2080df1e27" class="contenttype-folder">Users</span></li>'
+    var items = finderdata[path].items;
+    for (var i=0; i<items.length; i++) {
+        var item = items[i];
+        $.merge(html, [
+            '<li class="active-result" data-url="' + item.url + '" data-uid="' + item.uid + '">',
+            '<span class="contenttype-' + item.normalized_type + '">' + item.title + '</span>',
+            '</li>'
+            ]
         )
     }
     this.results.html(html.join(''));
@@ -41,6 +38,9 @@ ContentFinder.prototype.listdir = function(path) {
 
 
 $(document).ready(function () {
-    var finder = new ContentFinder('#related_items_finder', '/Plone');
-    finder.listdir('/Plone');
+    $('.finder').each(function() {
+        var url = $(this).attr('data-url');
+        var finder = new ContentFinder('#'+$(this).attr('id'), url);
+        finder.listdir(url);
+    });
 });
---
+++ clickdummies/contentfinder/finderdata.js
@@ -0,0 +1,106 @@
+var finderdata = {
+    "http://localhost:8080/Plone": {
+        "parent_url": "",
+        "path": [
+            {"url": "http://localhost:8080/Plone",
+            "icon": null,
+            "title": "Home"}
+            ],
+        "upload_allowed": false,
+        "items": [
+            {
+                "description": "Congratulations! You have successfully installed Plone.",
+                "uid": "811793d984af4c2dbdf4db2080df1e27",
+                "title": "Welcome to Plone",
+                "url": "http://localhost:8080/Plone/front-page",
+                "is_folderish": false,
+                "portal_type": "Document",
+                "icon": null,
+                "id": "front-page",
+                "normalized_type": "document"
+            },
+            {
+                "description": "Site News",
+                "uid": "ad59bb63e1d648fea9ec04818289a271",
+                "title": "News",
+                "url": "http://localhost:8080/Plone/news",
+                "is_folderish": true, "portal_type": "Folder",
+                "icon": null,
+                "id": "news",
+                "normalized_type": "folder"
+            },
+            {
+                "description": "Site Events",
+                "uid": "5e745db171614119af59be55ab12c425",
+                "title": "Events",
+                "url": "http://localhost:8080/Plone/events",
+                "is_folderish": true, "portal_type": "Folder",
+                "icon": null,
+                "id": "events",
+                "normalized_type": "folder"
+            },
+            {
+                "description": "Site Users",
+                "uid": "47cb04f38c9345f9a634c6c529382688",
+                "title": "Users",
+                "url": "http://localhost:8080/Plone/Members",
+                "is_folderish": true, "portal_type": "Folder",
+                "icon": null,
+                "id": "Members",
+                "normalized_type": "folder"
+            },
+        ]
+    },
+    "http://localhost:8080/Plone/news": {
+        "parent_url": "",
+        "path": [
+            {"url": "http://localhost:8080/Plone",
+            "icon": null,
+            "title": "Home"}
+            ],
+        "upload_allowed": false,
+        "items": [
+            {
+                "description": "Congratulations! You have successfully installed Plone.",
+                "uid": "811793d984af4c2dbdf4db2080df1e27",
+                "title": "Welcome to Plone",
+                "url": "http://localhost:8080/Plone/front-page",
+                "is_folderish": false,
+                "portal_type": "Document",
+                "icon": null,
+                "id": "front-page",
+                "normalized_type": "document"
+            },
+            {
+                "description": "Site News",
+                "uid": "ad59bb63e1d648fea9ec04818289a271",
+                "title": "News",
+                "url": "http://localhost:8080/Plone/news",
+                "is_folderish": true, "portal_type": "Folder",
+                "icon": null,
+                "id": "news",
+                "normalized_type": "folder"
+            },
+            {
+                "description": "Site Events",
+                "uid": "5e745db171614119af59be55ab12c425",
+                "title": "Events",
+                "url": "http://localhost:8080/Plone/events",
+                "is_folderish": true, "portal_type": "Folder",
+                "icon": null,
+                "id": "events",
+                "normalized_type": "folder"
+            },
+            {
+                "description": "Site Users",
+                "uid": "47cb04f38c9345f9a634c6c529382688",
+                "title": "Users",
+                "url": "http://localhost:8080/Plone/Members",
+                "is_folderish": true, "portal_type": "Folder",
+                "icon": null,
+                "id": "Members",
+                "normalized_type": "folder"
+            },
+        ]
+    },
+}
